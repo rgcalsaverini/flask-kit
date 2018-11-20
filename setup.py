6933e05ba0dc33c7766859ae8bcd046d8c9d1359
rgcalsaverini <rui.calsaverini@gmail.com>
2018-11-20 20:03:29 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='flask-kit',
-    version='0.0.1',
+    version='0.0.2',
     url='https://gitlab.com/rgcalsaverini/flask-kit',
     author='Rui Calsaverini',
     author_email='rui.calsaverini',
